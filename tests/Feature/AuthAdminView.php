f639486495dba56af923e4e1e09ea61878a2269e
fdh.luis.angel <fdh.luis.angel@gmail.com>
2022-04-22 12:17:16 -0500

    :sparkles: New feature :: Error test created

---
+++ tests/Feature/AuthAdminView.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
+class AuthAdminView extends TestCase
+{
+    /** @test */
+    public function views_admin_auth()
+    {
+        $userTodelete = DB::statement('delete from users where identification = 1085320421');
+
+        $user = User::create([
+            'name' => 'test',
+            'email' => 'test1@gmail.com',
+            'password' => '123',
+            'role_id' => 1,
+            'identification' => 1085320421,
+            'gender_id' => 1,   
+            'address' => 'Mz B Casa 30 STA Monica',
+            'phone' => '3183844053',
+            'verified_email' => true,
+        ]);
+
+        sleep(3); 
+
+        $response = $this->get('api/v1/users/1085320421');
+        $response->assertStatus(200);
+        $response->assertSee('1085320421');
+
+        $this->actingAs($user);
+
+        $response = $this->get(route('/home'));
+        $response->assertStatus(200);
+        $response->assertSee('Personas vacunadas según el tipo de vacuna');
+    }
+}
+
+//Personas vacunadas según el tipo de vacuna
